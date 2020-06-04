cf2cfa60db41f246a749018c3042e3bd24947d6b
Artem Demenko <ADemenko@titanmachinery.ua>
2020-06-05 00:10:15 +0300

    Homework-11

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const SIZE_SMALL = { price: 50, callories: 20 };
+const SIZE_MEDIUM = { price: 75, callories: 30 };
+const SIZE_BIG = { price: 100, callories: 40 };
+
+const TOPPING_CHEESE = { price: 10, callories: 20 };
+const TOPPING_SALAD = { price: 20, callories: 5 };
+const TOPPING_POTATO = { price: 15, callories: 10 };
+const TOPPING_CURRY = { price: 15, callories: 0 };
+const TOPPING_MAYO = { price: 10, callories: 20 };
+
+function Hamburger(base) {
+    this.price = base.price;
+    this.callories = base.callories;
+}
+
+Hamburger.prototype.addTopping = function (topping) {
+    this.price += topping.price;
+    this.callories += topping.callories;
+}
+
+Hamburger.prototype.getPrice = function () {
+    return this.price;
+}
+
+Hamburger.prototype.getCallories = function () {
+    return this.callories;
+}
+
+const hamburger = new Hamburger(SIZE_MEDIUM);
